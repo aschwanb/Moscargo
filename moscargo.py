dea6af7fda83e1664fd84a7390e42dc01577c8d7
Balz Aschwanden <balz.aschwanden@unibas.ch>
2017-07-07 10:24:05 +0200

    Read catalog name from route

--- moscargo.py
+++ moscargo.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 from flask import Flask
 from flask import render_template, redirect
 app = Flask(__name__)
@@ -8,8 +10,6 @@ import os
 
 #set this to the base of your munki repo:
 repo_base = os.environ.get('MOSCARGO_REPO') or '/Users/Shared/repo/'
-# override the catalog to parse below:
-catalog_to_parse = 'all'
 
 # yup, stolen whole-heartedly from http://stackoverflow.com/a/22878816/743638
 def get_key_watcher():
@@ -23,52 +23,55 @@ def get_key_watcher():
             return True  # key seen for the first time
     return key_not_seen
 
-try:
-    products = plistlib.readPlist(os.path.join(repo_base, 'catalogs', catalog_to_parse))
-    prodlist = []
-    for prod_dict in products:
-        if not prod_dict.get('installer_type') == 'apple_update_metadata':
-            if not prod_dict.get('installer_type') == 'nopkg':
-                joined_path = os.path.join(repo_base,'icons', prod_dict.get('name')) + '.png'
-                this_prod_dict = {}
-                try_keys = [('Name', 'display_name'), ('distinct_name', 'name')]
-                for item in try_keys:
-                    try:
-                        this_prod_dict[item[0]] = prod_dict.get(item[1])
-                    except Exception:
-                        this_prod_dict[item[0]] = 'No %s found' % item[0]
-                try:
-                    this_prod_dict['version'] = LooseVersion(prod_dict.get('version'))
-                except Exception:
-                    this_prod_dict['description'] = 'No description found'
-                try:
-                    this_prod_dict['description'] = (prod_dict.get('description'))[:130] + '...'
-                except Exception:
-                    this_prod_dict['description'] = 'No description found'
-                try:
-                    this_prod_dict['link'] = (os.path.join('static/pkgs', prod_dict.get('installer_item_location'))).replace(' ', '%20')
-                except Exception:
-                    this_prod_dict['link'] = 'No link!'
+def read_catalog(catalog_to_parse):
+	try:
+	    products = plistlib.readPlist(os.path.join(repo_base, 'catalogs', catalog_to_parse))
+	    prodlist = []
+	    for prod_dict in products:
+		if not prod_dict.get('installer_type') == 'apple_update_metadata':
+		    if not prod_dict.get('installer_type') == 'nopkg':
+			joined_path = os.path.join(repo_base,'icons', prod_dict.get('name')) + '.png'
+			this_prod_dict = {}
+			try_keys = [('Name', 'display_name'), ('distinct_name', 'name')]
+			for item in try_keys:
+			    try:
+				this_prod_dict[item[0]] = prod_dict.get(item[1])
+			    except Exception:
+				this_prod_dict[item[0]] = 'No %s found' % item[0]
+			try:
+			    this_prod_dict['version'] = LooseVersion(prod_dict.get('version'))
+			except Exception:
+			    this_prod_dict['description'] = 'No description found'
+			try:
+			    this_prod_dict['description'] = (prod_dict.get('description'))[:130] + '...'
+			except Exception:
+			    this_prod_dict['description'] = 'No description found'
+			try:
+			    this_prod_dict['link'] = (os.path.join('static/pkgs', prod_dict.get('installer_item_location'))).replace(' ', '%20')
+			except Exception:
+			    this_prod_dict['link'] = 'No link!'
 
-                if prod_dict.get('installer_type') == 'profile':
-                    this_prod_dict['icon_url'] = 'static/mobileconfig.png'
-                elif prod_dict.get('icon_name'):
-                    this_prod_dict['icon_url'] = (os.path.join('static/icons/', prod_dict.get('icon_name'))).replace(' ', '%20')
-                elif os.path.exists(joined_path):
-                    this_prod_dict['icon_url'] = (os.path.join('static/icons', prod_dict.get('name') + '.png')).replace(' ', '%20')
-                else:
-                    this_prod_dict['icon_url'] = 'static/package.png'
-                prodlist.append(this_prod_dict)
-    listbyvers = sorted(prodlist, key=itemgetter('version'), reverse=True)
-    filtered = filter(get_key_watcher(), listbyvers)
-    sprodlist = sorted(filtered, key=itemgetter('Name'))
+			if prod_dict.get('installer_type') == 'profile':
+			    this_prod_dict['icon_url'] = 'static/mobileconfig.png'
+			elif prod_dict.get('icon_name'):
+			    this_prod_dict['icon_url'] = (os.path.join('static/icons/', prod_dict.get('icon_name'))).replace(' ', '%20')
+			elif os.path.exists(joined_path):
+			    this_prod_dict['icon_url'] = (os.path.join('static/icons', prod_dict.get('name') + '.png')).replace(' ', '%20')
+			else:
+			    this_prod_dict['icon_url'] = 'static/package.png'
+			prodlist.append(this_prod_dict)
+	    listbyvers = sorted(prodlist, key=itemgetter('version'), reverse=True)
+	    filtered = filter(get_key_watcher(), listbyvers)
+	    sprodlist = sorted(filtered, key=itemgetter('Name'))
+	    return sprodlist
 
-except Exception, e:
-    print e
+	except Exception, e:
+	    print e
 
-@app.route('/')
-def index():
-    return render_template('moscargo.html', example_prods=sprodlist)
+@app.route('/<catalog>')
+def index(catalog):
+	sprodlist = read_catalog(catalog)
+	return render_template('moscargo.html', example_prods=sprodlist)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
